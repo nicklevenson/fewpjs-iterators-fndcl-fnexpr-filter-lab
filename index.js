1046382625825cd90beef90618b59b8a132b62a8
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-01 12:11:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter(d => {
+    return d.toLowerCase() === string.toLowerCase()
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(d => {
+    return d.charAt(0) === string.charAt(0)
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(d => {
+    return d.name === string
+  })
+}
+
